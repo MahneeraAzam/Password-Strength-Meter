585d341f0446a51aa6834d348322a5e4f7e79b93
MahneeraAzam <minheraazam403@gmail.com>
2025-03-06 16:45:35 +0500

    Fixed package and script name

--- src/pswrd-strenght-meter/__init__.py
+++ src/pswrd-strenght-meter/__init__.py
@@ -1,2 +1,9 @@
-def main() -> None:
-    print("Hello from pswrd-strenght-meter!")
+import streamlit as st
+
+def main():
+    st.title("Password Strength Meter")
+    st.write("Welcome to the Password Strength Checker!")
+
+if __name__ == "__main__":
+    main()
+
